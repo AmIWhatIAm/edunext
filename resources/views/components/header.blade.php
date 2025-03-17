d62516fcc65f99844d0bd0a2d855fcab7fdb4831
AmIWhatIAm <96875017+AmIWhatIAm@users.noreply.github.com>
2025-03-17 14:45:17 +0800

    Completed header component

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,15 +1,21 @@
-<div class="header">
-    <img src="{{ asset('image/logo_dark.svg') }}" alt="" class="header-logo">
-    <div>
-        <div class="header-links">
-            <a href="">Home</a>
-            <a href="">Courses</a>
-            <a href="">About Us</a>
-        </div>
-        <div>
-            <div></div>
-            <div>Username</div>
-            <div></div>
+<header>
+
+    <div class="header">
+        <img src="{{ asset('image/logo_dark.svg') }}" alt="" class="header-logo">
+        <div class="header-nav">
+            <div class="header-links">
+                <a href="">Home</a>
+                <a href="">Courses</a>
+                <a href="">About Us</a>
+            </div>
+            <div class="user-rectangle">
+                <div class="username">Username</div>
+                <div onclick="">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708" />
+                    </svg>
+                </div>
+            </div>
         </div>
     </div>
-</div>
+</header>

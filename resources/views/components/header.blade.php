df0219b7a79250776218415e63076bfa740c88ca
AmIWhatIAm <96875017+AmIWhatIAm@users.noreply.github.com>
2025-03-17 09:59:15 +0800

    Added header,footer and layout components, configured laravel mix, updated readme

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,15 @@
+<div class="header">
+    <img src="{{ asset('image/logo_dark.svg') }}" alt="" class="header-logo">
+    <div>
+        <div class="header-links">
+            <a href="">Home</a>
+            <a href="">Courses</a>
+            <a href="">About Us</a>
+        </div>
+        <div>
+            <div></div>
+            <div>Username</div>
+            <div></div>
+        </div>
+    </div>
+</div>
--- webpack.mix.js
+++ webpack.mix.js
@@ -10,8 +10,10 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+ mix.js('resources/js/app.js', 'public/js')
+ .postCss('resources/css/app.css', 'public/css', [
+     // Add any processors like autoprefixer if needed
+ ])
+ .options({
+     processCssUrls: false // Helps with relative paths in imports
+ });
